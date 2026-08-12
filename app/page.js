cd10b92acfdc476423ac7fcb71ded2e647d845d9
AdmiralGufi <bakyt130600@gmail.com>
2026-08-13 01:28:21 +0600

    Switch app to WMS v2 interface

--- app/page.js
+++ app/page.js
@@ -1,2 +1,2 @@
-import WmsApp from '../components/WmsApp';
-export default function Page(){return <WmsApp/>}
+import WmsAppV2 from '../components/WmsAppV2';
+export default function Page(){return <WmsAppV2/>}
