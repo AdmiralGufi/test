1e754adf8ccc992e0112c90c953aeeb0c1562c72
AdmiralGufi <bakyt130600@gmail.com>
2026-08-13 01:15:50 +0600

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Fulfillment WMS',description:'Операционная WMS для FBS-фулфилмента'};
+export default function RootLayout({children}){return <html lang="ru"><body>{children}</body></html>}
